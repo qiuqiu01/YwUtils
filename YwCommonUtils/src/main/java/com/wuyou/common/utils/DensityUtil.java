45057865286ae48abee0f5c224571ba7f188af1b
yanwen <690253704@qq.com>
2017-07-05 16:06:38 +0800

    <A>     * 添加设备密度转化的类DensityUtil.java     * 添加集合计算的类 GeometryUtils.java

---
+++ YwCommonUtils/src/main/java/com/wuyou/common/utils/DensityUtil.java
@@ -0,0 +1,49 @@
+package com.wuyou.common.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by QiuQiu on 2017-07-05.
+ */
+
+public class DensityUtil {
+    /**
+     * dp转px
+     * @return
+     */
+    public static int dp2px(Context context, float dpVal)
+    {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp转px
+     * @return
+     */
+    public static int sp2px(Context context, float spVal)
+    {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * px2dp
+     * @return
+     */
+    public static float px2dp(Context context, float pxVal)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (pxVal / scale);
+    }
+
+    /**
+     * px转sp
+     * @return
+     */
+    public static float px2sp(Context context, float pxVal)
+    {
+        return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+}
