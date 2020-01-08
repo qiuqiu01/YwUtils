665eccc95cf4f34e2412ce22ab2fb6d80b2dbed9
690253704@qq.com <ywaixw19880328yw>
2020-01-08 16:04:47 +0800

    <M>   * 修改ToastUtil，优化显示算法。

--- YwCommonUtils/src/main/java/com/wuyou/common/utils/ToastUtil.java
+++ YwCommonUtils/src/main/java/com/wuyou/common/utils/ToastUtil.java
@@ -4,16 +4,52 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToastUtil {
-	private static Toast toast;
+
 	/**
-	 * 强大的吐司，能够连续弹的吐司
-	 * @param text
+	 * 之前显示的内容
 	 */
-	public static void showToast(Context context, String text){
-		if(toast==null){
-			toast = Toast.makeText(context, text,Toast.LENGTH_SHORT);
+	private static String oldMsg;
+
+	/**
+	 * Toast对象
+	 */
+	private static Toast toast = null;
+
+	/**
+	 * 第一次时间
+	 */
+	private static long oneTime = 0;
+
+	/**
+	 * 第二次时间
+	 */
+	private static long twoTime = 0;
+
+	/**
+	 * 单例模式显示Toast,稍作优化
+	 * @param context
+	 * @param message
+	 */
+	public static void showToast(Context context, String message) {
+		if (toast == null) {
+			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+			toast.show();
+			// 记录第一次显示的时间
+			oneTime = System.currentTimeMillis();
+		} else {
+			// 获取第二次显示的时间
+			twoTime = System.currentTimeMillis();
+			if (message.equals(oldMsg)) {
+				// 优化显示，如果两次显示内容一样，则仅当两次时间间隔大于LENGTH_SHORT时才显示
+				if (twoTime - oneTime > Toast.LENGTH_SHORT) {
+					toast.show();
+				}
+			} else {
+				oldMsg = message;
+				toast.setText(message);
+				toast.show();
+			}
 		}
-		toast.setText(text);	// 如果不为空，则直接改变当前toast的文本
-		toast.show();
+		oneTime = twoTime;
 	}
 }
