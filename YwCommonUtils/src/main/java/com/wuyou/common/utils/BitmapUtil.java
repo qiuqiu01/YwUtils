8ec0454206ec9fad1af4b5c9178cc5754e8c56a0
690253704@qq.com <ywaixw19880328yw>
2020-01-08 16:05:49 +0800

    <A>   * 新增BitmapUtil，对Bitmap操作

---
+++ YwCommonUtils/src/main/java/com/wuyou/common/utils/BitmapUtil.java
@@ -0,0 +1,137 @@
+package com.wuyou.common.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+public class BitmapUtil {
+
+    /**
+     * 更改图片尺寸
+     * @param bitmap
+     * @param sx
+     * @param sy
+     * @return 修改后的Bitmap
+     */
+    public static Bitmap resize(Bitmap bitmap, float sx, float sy) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(sx, sy); // 长和宽放大缩小的比例
+        Bitmap resizeBmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        return resizeBmp;
+    }
+
+    /**
+     * 将Bitmap图片转换成圆形
+     * 切出来的圆形较大
+     * @param bitmap
+     * @return
+     */
+    public Bitmap toRoundBitmap(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        float roundPx;
+        float left,top,right,bottom,dst_left,dst_top,dst_right,dst_bottom;
+        if (width <= height) {
+            roundPx = width / 2;
+            top = 0;
+            bottom = width;
+            left = 0;
+            right = width;
+            height = width;
+            dst_left = 0;
+            dst_top = 0;
+            dst_right = width;
+            dst_bottom = width;
+        } else {
+            roundPx = height / 2;
+            float clip = (width - height) / 2;
+            left = clip;
+            right = width - clip;
+            top = 0;
+            bottom = height;
+            width = height;
+            dst_left = 0;
+            dst_top = 0;
+            dst_right = height;
+            dst_bottom = height;
+        }
+
+        Bitmap output = Bitmap.createBitmap(width,
+                height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect src = new Rect((int)left, (int)top, (int)right, (int)bottom);
+        final Rect dst = new Rect((int)dst_left, (int)dst_top, (int)dst_right, (int)dst_bottom);
+        final RectF rectF = new RectF(dst);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, src, dst, paint);
+        return output;
+    }
+
+    /**
+     * 图片转换成圆形
+     * 切出来的圆形很小
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap makeRoundCorner(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        int left = 0;
+        int top = 0;
+        int right = width;
+        int bottom = height;
+        float roundPx = height / 2;
+        if (width > height) {
+            left = (width - height) / 2;
+            top = 0;
+            right = left + height;
+            bottom = height;
+        } else if (height > width) {
+            left = 0;
+            top = (height - width) / 2;
+            right = width;
+            bottom = top + width;
+            roundPx = width / 2;
+        }
+
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        int color = 0xff424242;
+        Paint paint = new Paint();
+        Rect rect = new Rect(left, top, right, bottom);
+        RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    /**
+     * 裁剪图片
+     * @param bitmap
+     * @param size
+     * @return
+     */
+    public static Bitmap cutBitmap(Bitmap bitmap, int size) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Bitmap b = Bitmap.createBitmap(bitmap, (width - size) / 2, (height - size) / 2, size, size);
+        return b;
+    }
+}
