baaeffdd4ba8e37880ffa4c417cc12111ddea066
690253704@qq.com <ywaixw19880328yw>
2020-01-13 13:52:38 +0800

    <M>   * 定义内存相关常量大小KB/MB等；

---
+++ YwCommonLib/src/main/java/com/wuyou/constant/MemoryConstants.java
@@ -0,0 +1,28 @@
+package com.wuyou.constant;
+
+/**
+ * <pre>
+ *     author: YanWen
+ *     time  : 2020/01/13
+ *     desc  : 存储相关常量
+ * </pre>
+ */
+public final class MemoryConstants {
+
+    /**
+     * Byte 与 Byte 的倍数
+     */
+    public static final int BYTE = 1;
+    /**
+     * KB 与 Byte 的倍数
+     */
+    public static final int KB   = 1024;
+    /**
+     * MB 与 Byte 的倍数
+     */
+    public static final int MB   = 1048576;
+    /**
+     * GB 与 Byte 的倍数
+     */
+    public static final int GB   = 1073741824;
+}
