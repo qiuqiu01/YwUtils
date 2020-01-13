2fa6d9c3f895a8d9d49c8f4c070b6a54ecf1dad3
690253704@qq.com <ywaixw19880328yw>
2020-01-13 13:56:17 +0800

    <M>   * 定义时间相关常量（以毫秒为基准）；

---
+++ YwCommonLib/src/main/java/com/wuyou/constant/TimeConstants.java
@@ -0,0 +1,35 @@
+package com.wuyou.constant;
+
+/**
+ * <pre>
+ *     author: YanWen
+ *     time  : 2020/01/13
+ *     desc  : 时间相关常量
+ * </pre>
+ */
+public final class TimeConstants {
+    /**
+     * 毫秒与毫秒的倍数
+     */
+    public static final int MSEC = 1;
+
+    /**
+     * 秒与毫秒的倍数
+     */
+    public static final int SEC  = 1000;
+
+    /**
+     * 分与毫秒的倍数
+     */
+    public static final int MIN  = 60000;
+
+    /**
+     * 时与毫秒的倍数
+     */
+    public static final int HOUR = 3600000;
+
+    /**
+     * 天与毫秒的倍数
+     */
+    public static final int DAY  = 86400000;
+}
