7d0b967fc2567d80fd234db40631e4f3b8e3e240
690253704@qq.com <ywaixw19880328yw>
2020-01-13 22:11:53 +0800

    <A>   * 增加SD 卡相关工具类；

---
+++ YwCommonLib/src/main/java/com/wuyou/utils/SDCardUtils.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2020.
+ * Created by QiuQiu on 2020/01/13.
+ * All Rights Reserved.
+ */
+package com.wuyou.utils;
+
+import android.content.Context;
+import android.os.storage.StorageManager;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <pre>
+ *     author: YanWen
+ *     time  : 2020/01/13
+ *     desc  : SD 卡相关工具类
+ *     warning  : 部分功能需要先到Application类里实现初始化---- YwUtils.init(application);
+ * </pre>
+ */
+public final class SDCardUtils {
+
+    private SDCardUtils() {
+        throw new UnsupportedOperationException("U can't instantiate me...");
+    }
+
+    /**
+     * 判断 SD 卡是否可用
+     *
+     * @return true : 可用<br>false : 不可用
+     */
+    public static boolean isSDCardEnable() {
+        return !getSDCardPaths().isEmpty();
+    }
+
+    /**
+     * 获取 SD 卡路径
+     *
+     * @param removable true : 外置 SD 卡<br>false : 内置 SD 卡
+     * @return SD 卡路径
+     */
+    @SuppressWarnings("TryWithIdenticalCatches")
+    public static List<String> getSDCardPaths(final boolean removable) {
+        List<String> paths = new ArrayList<>();
+        StorageManager sm =
+                (StorageManager) YwUtils.getApp().getSystemService(Context.STORAGE_SERVICE);
+        try {
+            Class<?> storageVolumeClazz = Class.forName("android.os.storage.StorageVolume");
+            Method getVolumeList = StorageManager.class.getMethod("getVolumeList");
+            Method getPath = storageVolumeClazz.getMethod("getPath");
+            Method isRemovable = storageVolumeClazz.getMethod("isRemovable");
+            Object result = getVolumeList.invoke(sm);
+            final int length = Array.getLength(result);
+            for (int i = 0; i < length; i++) {
+                Object storageVolumeElement = Array.get(result, i);
+                String path = (String) getPath.invoke(storageVolumeElement);
+                boolean res = (Boolean) isRemovable.invoke(storageVolumeElement);
+                if (removable == res) {
+                    paths.add(path);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return paths;
+    }
+
+    /**
+     * 获取 SD 卡路径
+     *
+     * @return SD 卡路径
+     */
+    @SuppressWarnings("TryWithIdenticalCatches")
+    public static List<String> getSDCardPaths() {
+        StorageManager storageManager = (StorageManager) YwUtils.getApp()
+                .getSystemService(Context.STORAGE_SERVICE);
+        List<String> paths = new ArrayList<>();
+        try {
+            Method getVolumePathsMethod = StorageManager.class.getMethod("getVolumePaths");
+            getVolumePathsMethod.setAccessible(true);
+            Object invoke = getVolumePathsMethod.invoke(storageManager);
+            paths = Arrays.asList((String[]) invoke);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return paths;
+    }
+}
