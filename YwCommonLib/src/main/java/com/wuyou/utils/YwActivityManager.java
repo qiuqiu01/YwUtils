32e5620f086235f317405343b66e9cbd1e3d0320
690253704@qq.com <ywaixw19880328yw>
2020-01-13 14:32:58 +0800

    <M>   * 将原来ActivityUtils工具类的部分功能移植到这里，它主要activity的添加和删除，以及推出程序。

---
+++ YwCommonLib/src/main/java/com/wuyou/utils/YwActivityManager.java
@@ -0,0 +1,65 @@
+package com.wuyou.utils;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class YwActivityManager {
+
+    private static YwActivityManager instance;
+
+    private List<Activity> activities = new ArrayList<Activity>();
+
+    private YwActivityManager() {
+        throw new UnsupportedOperationException("Can't instantiate YwActivityManager..." +
+                " Consider use getInstance() instead!");
+    }
+
+    public static YwActivityManager getInstance() {
+        if (instance == null) {
+            synchronized (YwActivityManager.class) {
+                if (instance == null) {
+                    instance = new YwActivityManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 添加Activity
+     * @param activity
+     */
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    /**
+     * 移除Activity
+     * @param activity
+     */
+    public void removeActivity(Activity activity) {
+        try {
+            for (int i = 0; i < activities.size(); i++) {
+                Activity act = activities.get(i);
+                if (activity != null && activity == act) {
+                    activities.remove(i);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 退出程序
+     */
+    public void finishAll() {
+        for (Activity activity : activities) {
+            activity.finish();
+        }
+        activities.clear();
+    }
+}
