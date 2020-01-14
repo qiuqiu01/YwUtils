80d976713b0cb8be33b653d94cab785a90606938
690253704@qq.com <ywaixw19880328yw>
2020-01-14 14:45:56 +0800

    <A>   * 将MainActivity修改成TestActivity;

--- YwCommonLib/src/main/java/com/wuyou/TestActivity.java
+++ YwCommonLib/src/main/java/com/wuyou/TestActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "MainActivity";
+public class TestActivity extends AppCompatActivity {
+    public static final String TAG = "TestActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
     }
 }
